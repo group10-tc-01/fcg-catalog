d8e88f7a2efa70c2b3d0220cbc8e65a48b57cd15
rhuaannn <rhuanmarques1223@gmail.com>
2025-12-06 00:02:42 -0300

    feat: add configuration ef core library

---
+++ src/FCG.Catalog.Infrastructure.SqlServer/Persistence/Configurations/LibraryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using FCG.Catalog.Domain.Catalog.Entity.Libraries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FCG.Catalog.Infrastructure.SqlServer.Persistence.Configurations
+{
+    public class LibraryConfiguration : BaseConfiguration<Library>
+    {
+        public override void Configure(EntityTypeBuilder<Library> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("Libraries");
+
+            builder.Property(l => l.UserId)
+                .IsRequired();
+
+            builder.HasIndex(l => l.UserId)
+                .IsUnique()
+                .HasDatabaseName("IX_Libraries_UserId");
+
+            builder.HasMany(l => l.LibraryGames)
+                .WithOne()
+                .HasForeignKey("LibraryId")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Navigation(l => l.LibraryGames)
+                .UsePropertyAccessMode(PropertyAccessMode.Field);
+        }
+    }
+}
