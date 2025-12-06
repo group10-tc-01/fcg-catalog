63b471e219d6e208c8fbc3707f53b9d2cc3773a3
rhuaannn <rhuanmarques1223@gmail.com>
2025-12-06 11:30:42 -0300

    feat: add repository

---
+++ src/FCG.Catalog.Domain/Repositories/IReadOnlyGameRepository.cs
@@ -0,0 +1,13 @@
+﻿using FCG.Catalog.Domain.Catalog.Entity.Games;
+using FCG.Catalog.Domain.Enum;
+
+namespace FCG.Catalog.Domain.Repositories
+{
+    public interface IReadOnlyGameRepository
+    {
+        Task<Game?> GetByNameAsync(string name);
+        Task<Game?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+        IQueryable<Game?> GetAllWithFilters(string? name = null, GameCategory? category = null, decimal? minPrice = null, decimal? maxPrice = null);
+    }
+}
---
+++ src/FCG.Catalog.Domain/Repositories/IWriteOnlyGameRepository.cs
@@ -0,0 +1,10 @@
+﻿using FCG.Catalog.Domain.Catalog.Entity.Games;
+
+namespace FCG.Catalog.Domain.Repositories
+{
+    public interface IWriteOnlyGameRepository
+    {
+        public Task AddAsync(Game game);
+    
+    }
+}
