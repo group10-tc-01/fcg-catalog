e2ccb4e92dd50ad359e4eb7dfda729e19184dbbb
rhuaannn <rhuanmarques1223@gmail.com>
2025-12-26 22:15:03 -0300

    feaat: add endpoint delete in game.

--- src/FCG.Catalog.Domain/Repositories/Game/IReadOnlyGameRepository.cs
+++ src/FCG.Catalog.Domain/Repositories/Game/IReadOnlyGameRepository.cs
@@ -12,6 +12,7 @@ namespace FCG.Catalog.Domain.Repositories.Game
         Task<Catalog.Entity.Games.Game?> GetByNameAsync(string name);
         Task<Catalog.Entity.Games.Game?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
         Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+        Task Delete(Catalog.Entity.Games.Game game, CancellationToken cancellationToken = default);
         IQueryable<Catalog.Entity.Games.Game?> GetAllWithFilters(string? name = null, GameCategory? category = null, decimal? minPrice = null, decimal? maxPrice = null);
     }
 }
