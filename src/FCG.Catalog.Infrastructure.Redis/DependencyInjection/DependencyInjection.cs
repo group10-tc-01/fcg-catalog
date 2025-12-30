ed62db7759f0c6ce6905e450e0101a3e7b1f0103
rhuaannn <rhuanmarques1223@gmail.com>
2025-12-30 18:45:35 -0300

    Refactor: Caching redis in libraryusecase

--- src/FCG.Catalog.Infrastructure.Redis/DependencyInjection/DependencyInjection.cs
+++ src/FCG.Catalog.Infrastructure.Redis/DependencyInjection/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using FCG.Catalog.Infrastructure.Redis.Interface;
+using FCG.Catalog.Infrastructure.Redis.Redis;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace FCG.Catalog.Infrastructure.Redis
 {
@@ -11,11 +14,14 @@ namespace FCG.Catalog.Infrastructure.Redis
         {
             var connectionString = configuration.GetConnectionString("Redis");
 
+
             services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = connectionString;
                 options.InstanceName = "FCG_Catalog_";
             });
+
+            services.AddScoped<ICaching, Caching>();
 
             return services;
         }
---
+++ src/FCG.Catalog.Infrastructure.Redis/Interface/ICaching.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCG.Catalog.Infrastructure.Redis.Interface
+{
+    public interface ICaching
+    {
+        Task SetAsync(string key, string value);
+        Task<string> GetAsync(string key);
+        Task RemoveAsync(string key);
+    }
+}
