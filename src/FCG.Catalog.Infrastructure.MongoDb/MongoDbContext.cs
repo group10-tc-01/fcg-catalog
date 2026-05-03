75037d83e4fb6a303819ca69841a333da8071ff9
Marcelo M. Oliveira <31392975+marcel0liveira@users.noreply.github.com>
2026-05-02 21:51:28 -0300

    606, feat: criar `GameDetailDocument.cs` (BSON document) (#54)
    * 602: Feat(Adicionado projeto infra para MongoDB e o nuget correspondente ao EF)
    
    * 604: feat(Criado Dependency Injection para o projeto Infrastructure.MongoDb, ajustado nugets que estava com target para .net 9)
    
    * 605: feat(incluído a conection do mongo na appsettings: [dev, release])
    
    * fix: ajuste para fix no sonar
    
    * 605, fix: add exclude coverage
    
    * 605, feat: criado o GameDetailDocument.cs
    
    * 605, feat: ajuste em GameDetailDocument para comportar o que foi pedido em na task 484.
    
    * 608, feat: ajuste de versão do nuget EF.mongo
    
    * 604, fix: ajustes de conflitos do PR
    
    * 604, fix: CI - dependency vulnerability package.
    
    * 605, fix ajustes
    
    * 605, fix: error pipelines: elasticsearch e redis.
    
    ---------
    
    Co-authored-by: flaviojcf <flaviojcostafilho@gmail.com>

--- src/FCG.Catalog.Infrastructure.MongoDb/MongoDbContext.cs
+++ src/FCG.Catalog.Infrastructure.MongoDb/MongoDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using FCG.Catalog.Domain.Catalog.Entities.Games;
+using FCG.Catalog.Infrastructure.MongoDb.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
 using System.Diagnostics.CodeAnalysis;
 
 namespace FCG.Catalog.Infrastructure.MongoDb
@@ -8,10 +10,16 @@ namespace FCG.Catalog.Infrastructure.MongoDb
     public class MongoDbContext : DbContext
     {
         public DbSet<Game> Games { get; set; } = null!;
+        public DbSet<GameDetailDocument> GamesDetail { get; set; } = null!;
+        public DbSet<GameCacheEntity> GamesCache { get; set; } = null!;
 
-        public MongoDbContext(DbContextOptions<MongoDbContext> options) : base(options)
+        public MongoDbContext(DbContextOptions<MongoDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<GameDetailDocument>().ToCollection("games_detail");
+            modelBuilder.Entity<GameCacheEntity>().ToCollection("games_cache");
         }
     }
 }
