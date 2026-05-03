21facb1cccb68caa02e951fa3d0a52cc77786252
flaviojcf <flaviojcostafilho@gmail.com>
2026-05-02 22:38:55 -0300

    feat: Add mongo settings

--- src/FCG.Catalog.Infrastructure.MongoDb/DependencyInjection/DependencyInjection.cs
+++ src/FCG.Catalog.Infrastructure.MongoDb/DependencyInjection/DependencyInjection.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
-using System.Diagnostics.CodeAnalysis;
 
 namespace FCG.Catalog.Infrastructure.MongoDb.DependencyInjection
 {
